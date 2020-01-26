40ed57e4ca4a539dbcc30194a76b541421ca1b52
Dmytro <dmytroresler@gmail.com>
2020-01-26 17:21:47 +0100

    new nav bar for tablet & mobile responsive

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const navbBtn = document.querySelector('.nav-btn'),
+      phoneNavBtn = document.querySelector('.phone-nav-btn'),
       mainContainer = document.querySelector('.main-container'),
       navBar = document.querySelector('.nav-bar'),
+      phoneNavBar = document.querySelector('.phone-nav-bar'),
+      mainHead = document.querySelector('.main-head'),
       arrowBtn = document.querySelector('.arrow-btn');
 
       console.log(arrowBtn);
       
 
 navbBtn.addEventListener('mouseenter', function() {
-        this.classList.toggle('open');
+    this.classList.toggle('open');
 });
 
 navbBtn.addEventListener('mouseleave', function() {
@@ -20,10 +23,14 @@ navbBtn.addEventListener('click', function() {
     if (mainContainer.classList) {
         mainContainer.classList.toggle('main-container-open');
         navBar.classList.toggle('nav-bar-open');
+        phoneNavBar.classList.toggle('phone-nav-bar-open');
+        mainHead.classList.toggle('main-head-open');
     }
     else {
         mainContainer.classList.remove('main-container-open');
         navBar.classList.remove('nav-bar-open');
+        phoneNavBar.classList.remove('phone-nav-bar-open');
+        mainHead.classList.remove('main-head-open');
     }
 });
 
